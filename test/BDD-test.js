971fbde199e71be573ccaf8118765c8218f2ac4d
Matthias Kling <m.kling@sprax.eu>
2016-02-15 16:34:56 +0100

    implement std operators as methods on terminal BDDs (in addition to the impl in BDD.prototype, of course)

--- test/BDD-test.js
+++ test/BDD-test.js
@@ -6,50 +6,98 @@ const pa     = require('pimped-assert'),
 
 /* module under test: */
 const BDD = require('../lib/BDD'),
-      T   = BDD.True,
-      F   = BDD.False,
-      ite = BDD.ite,
-      not = BDD.not,
-      and = BDD.and,
-      or  = BDD.or,
-      eqv = BDD.eqv,
-      xor = BDD.xor,
-      imp = BDD.imp;
+      T    = BDD.True,
+      F    = BDD.False,
+      ite  = BDD.ite,
+      not  = BDD.not,
+      and  = BDD.and,
+      or   = BDD.or,
+      eqv  = BDD.eqv,
+      xor  = BDD.xor,
+      imp  = BDD.imp,
+      nand = BDD.nand,
+      nor  = BDD.nor;
+
+const truth_tables = {
+    ite: [  [F, F, F, F], // \
+            [F, F, T, T], // | project to
+            [F, T, F, F], // | 2nd arg
+            [F, T, T, T], // /
+            [T, F, F, F], // \
+            [T, F, T, F], // | project to
+            [T, T, F, T], // | 1st arg
+            [T, T, T, T], // /
+    ],
+    not: [  [F, T],
+            [T, F]
+    ],
+    and: [  [F, F, F],
+            [F, T, F],
+            [T, F, F],
+            [T, T, T]
+    ],
+    or:  [  [F, F, F],
+            [F, T, T],
+            [T, F, T],
+            [T, T, T]
+    ],
+    eqv: [  [F, F, T],
+            [F, T, F],
+            [T, F, F],
+            [T, T, T]
+    ],
+    xor: [  [F, F, F],
+            [F, T, T],
+            [T, F, T],
+            [T, T, F]
+    ],
+    imp: [  [F, F, T],
+            [F, T, T],
+            [T, F, F],
+            [T, T, T]
+    ],
+    nand: [ [F, F, T],
+            [F, T, T],
+            [T, F, T],
+            [T, T, F]
+    ],
+    nor: [  [F, F, T],
+            [F, T, F],
+            [T, F, F],
+            [T, T, F]
+    ],
+};
+
+refute.same(T, F, "BDD.True is different from BDD.False");
 
 
 () => {
-    refute.same(T, F, "BDD.True is different from BDD.False");
+    for (let opName in truth_tables) {
+        let op    = BDD[opName],
+            tt    = truth_tables[opName],
+            arity = tt[0].length - 1;
 
-    assert.same(ite(F, T, T), T);
-    assert.same(ite(F, T, F), F);
-    assert.same(ite(F, F, T), T);
-    assert.same(ite(F, F, F), F);
-    assert.same(ite(T, T, T), T);
-    assert.same(ite(T, T, F), T);
-    assert.same(ite(T, F, T), F);
-    assert.same(ite(T, F, F), F);
-
-    assert.same(F.neg, T);
-    assert.same(T.neg, F);
-
-    assert.same(F.not(), T);
-    assert.same(T.not(), F);
-}();
+        assert.typeof(op, "function", "module BDD provides a ." + opName + " function");
+        for (let i = 0; i < tt.length; i++) {
+            let line   = tt[i],
+                args   = line.slice(0, arity),
+                result = line[arity];
+            assert.same(op.apply(null, args), result,
+                "arity " + arity + ' operator ' + opName + " should map (" + args.join(', ') + ') to ' + result);
+        }
 
 
-() => {
-    [   "ite",
-        "not",
-        "and",
-        "or",
-        "eqv",
-        "xor",
-        "imp",
-        "nand",
-        "nor",
-    ].forEach(fname => {
-        assert.typeof(BDD[fname], "function", "BDD provides a ." + fname + " function");
-    });
+        assert.typeof(T[opName], "function", "BDD object T provides a ." + opName + " method");
+        assert.typeof(F[opName], "function", "BDD object F provides a ." + opName + " method");
+        for (let i = 0; i < tt.length; i++) {
+            let line     = tt[i],
+                invocant = line[0],
+                args     = line.slice(1, arity),    // arity of the operator, NOT the method (which gets the 1st arg as this)
+                result   = line[arity];
+            assert.same(invocant[opName].apply(invocant, args), result,
+                "arity " + (arity-1) + ' method .' + opName + ' on ' + invocant + " should map (" + args.join(', ') + ') to ' + result);
+        }
+    }
 }();
 
 /* BDD.size */
