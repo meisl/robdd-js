aa697510eaf57bd007659e9198c67925b87032ea
Matthias Kling <m.kling@sprax.eu>
2016-03-03 15:16:28 +0100

    add serialization

---
+++ test/BDD-serialization-test.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const util   = require('util');
+const pa     = require('pimped-assert'),
+      assert = pa.assert,
+      refute = pa.refute;
+
+const BDD = require('../lib/BDD'),
+      T      = BDD.True,
+      F      = BDD.False,
+      ite    = BDD.ite,
+      not    = BDD.not,
+      and    = BDD.and,
+      or     = BDD.or,
+      eqv    = BDD.eqv,
+      xor    = BDD.xor,
+      imp    = BDD.imp
+;
+const bitstr = require('../lib/BDD-bitstr').bitstr;
+
+
+/* module under test: */
+const BDDser = require('../lib/BDD-serialization'),
+      serialize   = BDDser.serialize,
+      deserialize = BDDser.deserialize
+;
+
+
+
+
+() => {
+    let s, p,
+        a = BDD.var('a'),
+        b = BDD.var('b'),
+        xs = bitstr('x', 2),
+        ys = bitstr('y', 2);
+
+    function check(p) {
+        let s = serialize(p);
+        console.log(p.size + "/" + p.toIteStr() + ":\n" + s.toString());
+        assert.same(deserialize(s), p, util.inspect(s));
+
+        let expected = Math.max(0, p.size - 2),
+            actual   = s.instructions.length / 4;
+        assert(actual <= expected, "should have " + expected + " or less instructions but has " + actual + ":\n" + util.inspect(s));
+    }
+
+    [
+        T, F,
+        a, b,
+        a.not, b.not,
+        and(a, b),
+        xor(a, b),
+        xs.eq(ys),
+    ].forEach(check);
+}();
+
