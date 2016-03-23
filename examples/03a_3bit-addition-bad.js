1e0980a2ff58a1499434a6616095ce6065f2df90
Matthias Kling <m.kling@sprax.eu>
2016-03-23 21:27:16 +0100

    redo examples/03a_3bit-addition using new bitstr_from_str_suffix

--- examples/03a_3bit-addition-bad.js
+++ examples/03a_3bit-addition-bad.js
@@ -11,24 +11,17 @@ const BDD    = require('../lib/BDD'),
       gv     = require('../lib/BDD-gv'),
       bitstr = require('../lib/BDD-bitstr').bitstr;
 
-let bitLen = 4,
-    a = bitstr(BDD.var('0a'), BDD.var('1a'), BDD.var('2a'), BDD.var('3a')),    //      bitstr('a', bitLen),    //
-    b = bitstr(BDD.var('0b'), BDD.var('1b'), BDD.var('2b'), BDD.var('3b')),    //      bitstr('b', bitLen),    //
-    c = bitstr(BDD.var('0c'), BDD.var('1c'), BDD.var('2c'), BDD.var('3c')),    //      bitstr('c', bitLen),    //
-    p;
+let bitLen = 8,
+    badA = bitstr('a', bitLen),
+    badB = bitstr('b', bitLen),
+    badC = bitstr('c', bitLen),
+    bad = badA.plus(badB).eq(badC),
+    goodA = bitstr(bitLen, 'a'),
+    goodB = bitstr(bitLen, 'b'),
+    goodC = bitstr(bitLen, 'c'),
+    good = goodA.plus(goodB).eq(goodC);
 
-let bit0   = xor(a[0], b[0]),   // bit0 is 1  iff  exactly one of a[0], b[0] is 1
-    carry0 = and(a[0], b[0]),
-    bit1   = ite(carry0, eqv(a[1], b[1]), xor(a[1], b[1])),
-    carry1 = ite(carry0, or( a[1], b[1]), and(a[1], b[1])),
-    bit2   = ite(carry1, eqv(a[2], b[2]), xor(a[2], b[2])),
-    carry2 = ite(carry1, or( a[2], b[2]), and(a[2], b[2])),
-    bit3   = ite(carry2, eqv(a[3], b[3]), xor(a[3], b[3])),
-    carry3 = ite(carry2, or( a[3], b[3]), and(a[3], b[3]))
-;
+console.log(bad.size);
 
-//p = carry3;
-p = eqv(c[0], bit0).and(eqv(c[1], bit1)).and(eqv(c[2], bit2)).and(eqv(c[3], bit3));
-
-
-gv.render(p);
+console.log(good.size);
+gv.render(good);
